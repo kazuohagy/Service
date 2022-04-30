7935e146a637f7eaf26efbd1c82ac4c0589f91da
Gustavo Kazuo <kazucalado@gmail.com>
2022-04-30 19:09:29 -0300

    services

---
+++ src/service/ContractService.java
@@ -0,0 +1,13 @@
+package service;
+
+import entities.Contract;
+
+public class ContractService {
+    public ContractService() {
+
+    }
+
+    public void processContract(Contract contract, Integer months) {
+
+    }
+}
