33c46c77fd3f9def380cd5e86ec87107d67e930f
Gustavo Kazuo <kazucalado@gmail.com>
2022-04-30 21:11:32 -0300

    [PaypalService] tristeza absoluta

--- src/service/PaypalService.java
+++ src/service/PaypalService.java
@@ -4,11 +4,11 @@ public class PaypalService implements OnlinePaymentService {
     public static final double FIXED_FEE = 0.02;
     public static final double MONTHLY_INTEREST = 0.01;
 
-    public Double paymentFee(Double amount) {
+    public double paymentFee(double amount) {
         return amount * FIXED_FEE;
     }
 
-    public Double interest(Double amount, Integer months) {
+    public double interest(double amount, Integer months) {
         return amount * MONTHLY_INTEREST * months;
     }
 }
