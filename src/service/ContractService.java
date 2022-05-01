2399f481b93468c01a0fdf2ae6f54598dd7555dc
Gustavo Kazuo <kazucalado@gmail.com>
2022-04-30 21:10:39 -0300

    [ContractService] criado

--- src/service/ContractService.java
+++ src/service/ContractService.java
@@ -1,13 +1,39 @@
 package service;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import entities.Contract;
+import entities.Installment;
 
 public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
     public ContractService() {
 
     }
 
     public void processContract(Contract contract, Integer months) {
+        double basicQuota = contract.getTotalValue() / months;
+        for (int i = 1; i <= months; i++) {
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota);
+            double total = interest + fee + basicQuota;
 
+            Date dueDate = addMonths(contract.getDate(), i);
+            contract.getInstallments().add(new Installment(dueDate, total));
+
+        }
+    }
+
+    private Date addMonths(Date date, int months) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, months);
+        return calendar.getTime();
     }
 }
