44840e820c996b62ee0498f837e9890d8ef8a2dd
Gustavo Kazuo <kazucalado@gmail.com>
2022-04-30 21:10:04 -0300

    [APP] for each

--- src/App.java
+++ src/App.java
@@ -1,28 +1,40 @@
 
-// import java.text.SimpleDateFormat;
-// import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
-import service.OnlinePaymentService;
+import entities.Contract;
+import entities.Installment;
+import service.ContractService;
 import service.PaypalService;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        // SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         System.out.println("Enter contract data:");
         System.out.print("Number:");
-        // Integer contractData = sc.nextInt();
+        Integer contractData = sc.nextInt();
         System.out.print("Date (dd/MM/yyyy):");
-        // Date date = sdf.parse(sc.next());
+        Date date = sdf.parse(sc.next());
         System.out.print("Contract value:");
-        // Double value = sc.nextDouble();
+        Double value = sc.nextDouble();
+
+        Contract contract = new Contract(contractData, date, value);
+
+        ContractService contractService = new ContractService(new PaypalService());
+
         System.out.print("Enter number of installments:");
-        // Integer installments = sc.nextInt();
-        System.out.println("Installments:");
-        OnlinePaymentService onlinePaymentService = new PaypalService();
-        System.out.println(onlinePaymentService.interest(200.0, 3));
+        Integer installments = sc.nextInt();
+
+        contractService.processContract(contract, installments);
+
+        for (Installment installment : contract.getInstallments()) {
+            System.out.println(installment);
+        }
         sc.close();
     }
 }
